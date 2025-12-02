4302cd50452952a55b2d3d4c17834b324304b2b5
fahimmuntacher <muntacherfahim234@gmail.com>
2025-12-02 16:37:21 +0600

    put and get api done

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
-import { Pool } from "pg";
-import dotenv from "dotenv"
-import path from "path"
+import { Pool, Result } from "pg";
+import dotenv from "dotenv";
+import path from "path";
 const app = express();
-dotenv.config({path : path.join(process.cwd(), ".env")})
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 const port = 5000;
 const pool = new Pool({
   connectionString: `${process.env.CONNECTION_STRING}`,
@@ -24,16 +24,15 @@ const initDb = async () => {
         `);
 
   await pool.query(`
-        CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
             )
             `);
 };
@@ -47,12 +46,102 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello first server!");
 });
 
-app.post("/", (req: Request, res: Response) => {
-  console.log(req.body);
+// users CRUD
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+    console.log(result);
+    res.send({ message: "data inserted", data: result.rows[0] });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
   res.status(201).json({
     success: true,
     message: "API is walking",
   });
+});
+
+// USERS GET API
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      message: "users retrived successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      details: err,
+    });
+  }
+});
+
+// users get by id
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id  = $1`, [
+      req.params.id,
+    ]);
+    if (result.rows.length === 0) {
+      res.status(401).json({
+        success: false,
+        message: "user not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User fetched successfully",
+        data: result.rows[0],
+      });
+    }
+    console.log(result.rows);
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  console.log(name, email);
+  try {
+    const result = await pool.query(
+      `UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
+      [name, email, req.params.id]
+    );
+    if (result.rows.length === 0) {
+      res.status(401).json({
+        success: false,
+        message: "user not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+        data: result.rows[0],
+      });
+    }
+    console.log(result.rows);
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 app.listen(port, () => {
